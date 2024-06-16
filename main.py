0622e7b1215a4abd6ca582b3487f9ebb134bc963
OtavioCanedo <otavio.canedo@soitic.com>
2024-06-16 12:00:19 -0300

    feat: treinamento e formação de palavras

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
 import cv2
 import mediapipe as mp
-from tensorflow.python.keras.models import load_model
 import numpy as np
+import tensorflow as tf
 
+np.set_printoptions(suppress=True)
+hands = mp.solutions.hands.Hands(max_num_hands=1)
+model = tf.saved_model.load('savedmodel')
+
+@tf.function
+def predict(image):
+    image = tf.expand_dims(image, 0)
+    predictions = model(image)
+    return predictions
+
+classes = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'I', 'L', 'M', 'N', 'O', 'R', 'S', 'T', 'U', 'V']
+data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 cap = cv2.VideoCapture(0)
 
-hands = mp.solutions.hands.Hands(max_num_hands=1)
-
-classes = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-# model = load_model('keras_model.h5')
-data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+word = ''
+last_letter = ''
+min_duration = 2000
+start_time = 0
+end_time = 0
 
 while True:
     success, img = cap.read()
-    frameRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    img_flipped = cv2.flip(img, 1)
+    frameRGB = cv2.cvtColor(img_flipped,cv2.COLOR_BGR2RGB)
     results = hands.process(frameRGB)
     handsPoints = results.multi_hand_landmarks
-    h, w, _ = img.shape
+    h, w, _ = img_flipped.shape
 
     if handsPoints != None:
         for hand in handsPoints:
@@ -34,21 +47,60 @@ while True:
                     y_max = y
                 if y < y_min:
                     y_min = y
-            cv2.rectangle(img, (x_min-50, y_min-50), (x_max+50, y_max+50), (0, 255, 0), 2)
+            cv2.rectangle(img_flipped, (x_min-50, y_min-50), (x_max+50, y_max+50), (0, 255, 0), 2)
 
             try:
-                imgCrop = img[y_min-50:y_max+50,x_min-50:x_max+50]
-                imgCrop = cv2.resize(imgCrop,(224,224))
+                imgCrop = img_flipped[y_min-50:y_max+50,x_min-50:x_max+50]
+                imgCrop = cv2.resize(imgCrop,(224,224), interpolation=cv2.INTER_AREA)
                 imgArray = np.asarray(imgCrop)
                 normalized_image_array = (imgArray.astype(np.float32) / 127.0) - 1
                 data[0] = normalized_image_array
-                # prediction = model.predict(data)
-                # indexVal = np.argmax(prediction)
-                #print(classes[indexVal])
-                # cv2.putText(img,classes[indexVal],(x_min-50,y_min-65),cv2.FONT_HERSHEY_COMPLEX,3,(0,0,255),5)
+
+                prediction = predict(normalized_image_array)
+                indexVal = np.argmax(prediction)
+
+                current_letter = classes[indexVal]
+
+                if current_letter != last_letter:
+                    start_time = cv2.getTickCount()
+                    last_letter = current_letter
+
+                end_time = cv2.getTickCount()
+                duration = (end_time - start_time) / cv2.getTickFrequency() * 1000
+
+                if duration >= min_duration: 
+                    word += current_letter
+                    last_letter = current_letter
+                    start_time = cv2.getTickCount()
+
+                if last_letter:
+                    cv2.putText(img_flipped,classes[indexVal],(x_min-50,y_min-65),cv2.FONT_HERSHEY_COMPLEX,3,(0,0,255),5)
+
+                if word:
+                    cv2.putText(img_flipped, word, (10, h - 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
 
             except:
                 continue
 
-    cv2.imshow('Imagem',img)
-    cv2.waitKey(1)
+    elif word:
+        cv2.putText(img_flipped, word, (10, h - 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+
+    key = cv2.waitKey(1) & 0xFF
+
+    if key == 32:
+        word += " "
+
+    if key == 8:
+        if len(word) > 0:
+            word = word[:-1]
+    
+    if key == 27:  
+        break
+
+    cv2.imshow('Imagem', img_flipped)
+
+    if cv2.getWindowProperty('Imagem', cv2.WND_PROP_VISIBLE) < 1:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
